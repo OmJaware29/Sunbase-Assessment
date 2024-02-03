e5908ff968e83671da913f3cb7a481bc4686e2fc
Rohit Yadav <60982731+java4rohit@users.noreply.github.com>
2024-02-03 22:51:17 +0530

    Add files via upload
    backend Project

---
+++ sunbase_Application/sunbase/src/main/java/com/crud/sunbase/SunbaseApplication.java
@@ -0,0 +1,12 @@
+package com.crud.sunbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class SunbaseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SunbaseApplication.class, args);
+	}
+}
